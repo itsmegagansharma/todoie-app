a2e9045ba70c3c216460d8f854b5f6272ea2a6ee
itsmegagansharma <itsmegagansharma@gmail.com>
2024-04-11 16:04:06 +0530

    Minor Changes related to List Variable

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import Header from "./Components/Header";
 import TodoItem from "./Components/TodoItem";
 import MyButton from "./Components/MyButton";
 // import UseStateHookPractice from "./Components/UseStateHookPractice";
-import UseEffectHookPractice from "./Components/UseEffectHookPractice";
+//import UseEffectHookPractice from "./Components/UseEffectHookPractice";
 
 function App() {
+  let todoList = [
+    { taskName: "Eat", complete: false },
+    { taskName: "Code", complete: false },
+    { taskName: "Sleep", complete: false },
+    { taskName: "Repeat", complete: false },
+  ];
+
   return (
     <div className="app-container">
       {/* <UseStateHookPractice /> */}
       {/* <UseEffectHookPractice /> */}
       <header>
-        <Header title="Todo App"/>
+        <Header title="Todo App" />
       </header>
-        <div>
-        <TodoItem taskName = "Eat"/>
-        <TodoItem taskName = "Code" complete = {false}/>
-        <TodoItem taskName = "Sleep"/>
-        <TodoItem taskName = "Repeat"/>
-        </div>
+      <div>
+        { todoList.map((todo, index) => (
+          <TodoItem
+            key={index}
+            taskName={todo.taskName}
+            complete={todo.complete}
+          />
+        ))}
+      </div>
       <footer>
         <MyButton />
       </footer>
